d2eb540b07e19fa1cd57162a4422d5f523e8d7cd
lonerthefirst3-sudo <lonerthefirst3@gmail.com>
2026-06-18 18:26:58 +0000

    refactor: extract shared Stellar config constants

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const RPC_URL = 'https://soroban-testnet.stellar.org'
+export const POOL_CONTRACT = 'CCSA4Q3DZ3FGABTATGWKE3EMNT6YKTUJNI7JACDX4336FJCYJJIG3KGW'
+export const DENOMINATION = 10_000_000n // 1 XLM in stroops
+export const NETWORK = 'TESTNET'
+export const EXPLORER_BASE = 'https://stellar.expert/explorer/testnet'
